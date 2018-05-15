4931d1d003309faa6f7e26cb7189cfacfa483d88
no1harm <chen09869@gmail.com>
2018-05-15 18:50:50 +0800

    进度1

--- task0002/js/task0002_3.js
+++ task0002/js/task0002_3.js
@@ -2,6 +2,7 @@ var count = 0;               //滚动的次数
 var whereGo = false;         //滚动的执行方向
 var timer ;                  //定义计时器对象
 
+
 var ul_img = document.getElementsByClassName("ul_img")[0];
 var li_img = document.getElementsByClassName("li_img");
 
@@ -23,4 +24,28 @@ function showtime() {
             }
         }
     },3000);
+}
+
+var arrow = document.getElementsByClassName("arrow");
+for (i = 0; i <arrow.length; i++) {
+    arrow[i].onmouseover = function () {
+        clearInterval(timer);
+    }
+    arrow[i].onmouseout = function() {
+        showtime()
+    }
+    arrow[i].onclick = function () {
+        if (this.title == 0){
+            count++;
+            if(count > 3){
+                count = 0;
+            }
+        } else {
+            count--;
+            if(count <3){
+                count = 3;
+            }
+        }
+        ul_img.style.transform = "translate(" + -800 * count + "px)";
+    }
 }
